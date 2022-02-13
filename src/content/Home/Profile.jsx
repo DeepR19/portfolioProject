e71710a22c706d10cf754cc7d4f71dbd26a86d8c
DeepR19 <chauhandeep5432@gmail.com>
2022-02-13 16:40:16 +0530

    Make profile page by -- react-typing react-fontawesome

--- src/content/Home/Profile.jsx
+++ src/content/Home/Profile.jsx
@@ -1,30 +1,84 @@
 import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {
+    faGithub,
+    faLinkedin,
+    faTelegram,
+    faWhatsapp,
+    faYoutube} from '@fortawesome/free-brands-svg-icons'
+
+import Typical from 'react-typical';
+
+import "./Profile.css"
+
 
 export default function Profile() {
   return (
     <div className="profile-container">
-    <div className="profile-parent">
-        <div className="profile-details">
-            <div className="colz">
-                <a href="#">
-                    <i className="fa fa-facebook-square"></i>
-                </a>
-                <a href="#">
-                    <i className="fa fa-google-plus-square"></i>
-                </a>
-                <a href="#">
-                    <i className="fa fa-youtube-square"></i>
-                </a>
-                <a href="#">
-                    <i className="fa fa-instagram"></i>
-                </a>
-                <a href="#">
-                    <i className="fa fa-twitter"></i>
-                </a>
+        <div className="profile-parent">
+            <div className="profile-details">
+                <div className="colz">
+                    <div className="colz-icons">
+                        <FontAwesomeIcon icon={faYoutube} className='ico' />
+                        <FontAwesomeIcon icon={faGithub} className='ico' />
+                        <FontAwesomeIcon icon={faLinkedin} className='ico' />
+                        <FontAwesomeIcon icon={faTelegram} className='ico' />
+                        <FontAwesomeIcon icon={faWhatsapp} className='ico'/>
+
+                    </div>
+                </div>
+
+
+                <div className="profile-details-name">
+                    <span className="primary-text">
+                        Hello, I'm <span className="highlighted-text">Deepak</span>
+                    </span>
+                </div>
+
+                <div className="profile-details-role">
+                    <span className="primary-text">
+                        <h1>
+                            <Typical
+                                loop={Infinity}
+                                steps={[
+                                    "Enthusiastic Dev 🔴",
+                                    1000,
+
+                                    "Full Stack Developer 😎",
+                                    1000,
+
+                                    "MERN Stack Developer 📱",
+                                    1000,
+
+                                    "React/React Native Dev 🌐",
+                                    1000
+                                ]}
+                            />
+                        </h1>
+
+                        <span className="profile-role-tagline">
+                            Knack of building applications with frontEnd and BackEnd operations...
+                        </span>
+                    </span>
+                </div>
+
+                <div className="profile-options">
+                    <button className="btn primary-btn">
+                        Hire Me
+                    </button>
+
+                    <a href="DeepakCV.pdf" download='DeepakCV.pdf'>
+                        <button className="btn highlighted-btn">GetResume</button>
+                    </a>
+                </div>
+
+            </div>
+
+            <div className="profile-picture">
+                <div className="profile-picture-background"></div>
             </div>
         </div>
     </div>
-</div>
   )
 }
 
